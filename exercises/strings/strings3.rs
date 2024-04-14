7049280e1b00ff1134afba99dd9a7cf1a1097239
NicotineIchiro <h_rer@foxmail.com>
2024-04-14 21:38:10 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,43 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let rets: String = String::from(input); // note: &str is immutable type!
+    let mut l: usize = 0;
+    let mut r: usize = 0;
+    let mut lb: bool = false;
+    let mut i: usize = 0;
+
+    for ch in (rets.clone()).chars() { //String cannot be directly access byte!
+        if ch != ' ' {
+            if lb == false {
+                l = i;
+                lb = true;
+            } else {
+                r = i;
+            }
+        }
+        i += 1;
+    }
+      
+    String::from(&input[l..=r])
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut rets = String::from(input);
+    rets.push_str(" world!");
+    rets
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut rets = String::from(input);
+    rets = rets.replace("cars", "balloons");
+    rets
 }
 
 #[cfg(test)]
